ade428dcd19e6cc065ae82fb1ab841cd4a18362d
HARDIKKUMAR B. KANGASIYA <hkangsia@gmail.com>
2024-05-31 15:28:52 +0530

    Add project files.

---
+++ HRManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HRManager.Startup))]
+namespace HRManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
